4c9c75a46d4a0931ca5fbe88bcfc2337c3c44d18
Manuel Spínola <mspinola10@gmail.com>
2026-05-19 17:58:01 -0600

    fix: renv.lock e imports.R actualizados

--- R/imports.R
+++ R/imports.R
@@ -4,7 +4,6 @@
 #' @import shinyjs
 #' @import ggplot2
 #' @import dplyr
-#' @import tidyr
 #' @import sf
 #' @importFrom DT DTOutput renderDT datatable
 #' @importFrom readr read_delim read_csv
@@ -15,20 +14,4 @@
 #' @importFrom tidyterra geom_spatraster
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom viridisLite viridis
-#' @importFrom scales percent comma
-#' @importFrom tibble tibble as_tibble
-#' @importFrom parameters model_parameters
-#' @importFrom performance model_performance
-#' @importFrom modelbased estimate_means
-#' @importFrom datawizard standardize
-#' @importFrom see plot
-#' @importFrom rsample initial_split
-#' @importFrom recipes recipe
-#' @importFrom parsnip linear_reg
-#' @importFrom workflows workflow
-#' @importFrom tune tune_grid
-#' @importFrom yardstick rmse
-#' @importFrom unmarked unmarkedFrameOccu
-#' @importFrom car Anova
-#' @importFrom MASS stepAIC
 NULL
