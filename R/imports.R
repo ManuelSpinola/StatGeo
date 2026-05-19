996fccd8219cbb4f06c24785ede8987337fc8b9f
Manuel Spínola <mspinola10@gmail.com>
2026-05-19 17:35:56 -0600

    fix: imports.R

---
+++ R/imports.R
@@ -0,0 +1,34 @@
+#' @import shiny
+#' @import bslib
+#' @import bsicons
+#' @import shinyjs
+#' @import ggplot2
+#' @import dplyr
+#' @import tidyr
+#' @import sf
+#' @importFrom DT DTOutput renderDT datatable
+#' @importFrom readr read_delim read_csv
+#' @importFrom readxl read_excel
+#' @importFrom terra rast plot
+#' @importFrom raster raster
+#' @importFrom mapview mapview
+#' @importFrom tidyterra geom_spatraster
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom viridisLite viridis
+#' @importFrom scales percent comma
+#' @importFrom tibble tibble as_tibble
+#' @importFrom parameters model_parameters
+#' @importFrom performance model_performance
+#' @importFrom modelbased estimate_means
+#' @importFrom datawizard standardize
+#' @importFrom see plot
+#' @importFrom rsample initial_split
+#' @importFrom recipes recipe
+#' @importFrom parsnip linear_reg
+#' @importFrom workflows workflow
+#' @importFrom tune tune_grid
+#' @importFrom yardstick rmse
+#' @importFrom unmarked unmarkedFrameOccu
+#' @importFrom car Anova
+#' @importFrom MASS stepAIC
+NULL
